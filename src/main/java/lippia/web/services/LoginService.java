1b090752353b7e65dee59ed32450c680a6ab0712
Gonzalo Soriano <gonzalo.soriano@crowdaronline.com>
2021-10-29 21:14:04 -0300

    Agrego comentarios a cada metodo de los services

--- src/main/java/lippia/web/services/LoginService.java
+++ src/main/java/lippia/web/services/LoginService.java
@@ -6,18 +6,33 @@ import lippia.web.constants.LoginConstants;
 
 public class LoginService extends ActionManager {
 
+    /**
+     * Este metodo llama a una funcion de Lippia, que está en la clase WebActionManager, llamada navigateTo
+     * y nos lleva hasta la url solicitada
+     */
     public static void navegarAlLogin(){
         WebActionManager.navigateTo("http://automationpractice.com/index.php?controller=authentication&back=my-account");
     }
 
+    /**
+     * Este metodo recibe un string que contiene un email enviado desde los steps(en este caso LoginSteps),
+     * y lo que hace es setear ese texto en el input email ubicado en la pagina de Login
+     */
     public static void setEmail(String email){
       setInput(LoginConstants.INPUT_EMAIL_CSS, email);
     }
 
+    /**
+     * Este metodo recibe un string que contiene una contraseña enviada desde los steps (en este caso LoginSteps),
+     * y lo que hace es setear ese texto en el input email ubicado en la pagina de Login
+     */
     public static void setPassword(String password){
         setInput(LoginConstants.INPUT_PASSWORD_CSS, password);
     }
 
+    /**
+     * Este metodo hace click en el boton SignIn ubicado en la pagina de login
+     */
     public static void clickSignIn(){
         click(LoginConstants.BUTTON_SIGNIN_CSS);
     }
--- src/main/java/lippia/web/services/MyAccountService.java
+++ src/main/java/lippia/web/services/MyAccountService.java
@@ -6,6 +6,12 @@ import org.testng.Assert;
 
 public class MyAccountService extends ActionManager {
 
+    /**
+     * Este metodo valida que el texto extraido del titulo de la pagina My Account, es efectivamente el texto "MY ACCOUNT"
+     *
+     * Se agrega un waitVisibility para que Lippia espere a que el elemento que solicitamos (TITLE_MYACCOUNT_CSS)
+     * se muestre en pantalla antes de realizar el paso de la validacion
+     */
     public static void validarMyAccount(){
         waitVisibility(MyAccountConstants.TITLE_MYACCOUNT_CSS);
         Assert.assertEquals(getText(MyAccountConstants.TITLE_MYACCOUNT_CSS), "MY ACCOUNT");
